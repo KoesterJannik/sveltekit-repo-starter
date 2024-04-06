5e6bd599d091eabef88682066427448abeac7c36
Jannik Köster <koesterjannik1998@gmail.com>
2024-04-06 11:07:27 +0200

    fix pipeline

--- src/lib/shared/modules/mailer/index.ts
+++ src/lib/shared/modules/mailer/index.ts
@@ -1,7 +1,5 @@
-import { SMTPEmailService } from './smtp-mailer';
 import { AWSEmailService } from './aws-mailer';
-import { PRIVATE_EMAIL_PROVIDER } from '$env/static/private';
 
-const mailer = PRIVATE_EMAIL_PROVIDER === 'aws' ? new AWSEmailService() : new SMTPEmailService();
+const mailer = new AWSEmailService();
 
 export default mailer;
