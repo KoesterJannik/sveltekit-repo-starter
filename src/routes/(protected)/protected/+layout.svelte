4918c62cc307511d7b4334fe00040f4816ec88d0
sean <135666428+seantiz@users.noreply.github.com>
2024-04-29 09:30:38 +0100

    Define User model type explicitly in layout AppShell instance

--- src/routes/(protected)/protected/+layout.svelte
+++ src/routes/(protected)/protected/+layout.svelte
@@ -1,6 +1,7 @@
-<script>
+<script lang="ts">
 	import AppShell from '../../../lib/components/layout/AppShell.svelte';
-	export let data;
+	import type { User } from '@prisma/client';
+	export let data: { user: User };
 </script>
 
 <AppShell user={data?.user}>
